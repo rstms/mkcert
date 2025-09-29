6e649de93a673a2751954ebe88ac8c4a6ea68a0c
Matt Krueger <mkrueger@rstms.net>
2025-09-29 10:57:23 -0600

    Bump version: 0.2.14 → 0.2.15

--- cmd/root.go
+++ cmd/root.go
@@ -42,7 +42,7 @@ var cfgFile string
 var certFactory *factory.CertFactory
 
 var rootCmd = &cobra.Command{
-	Version: "0.2.14",
+	Version: "0.2.15",
 	Use:     "mkcert [flags] SUBJECT [-- STEP-OPTS]",
 	Short:   "make client certificate",
 	Long: `
